40d7d5c75f376c5a8756b7fd4d092db19f11b31d
Troy <troyswanson1010@gmail.com>
2023-08-10 02:07:09 -0500

    start

---
+++ TMSpeedPatcher/Settings.cs
@@ -0,0 +1,22 @@
+﻿using Mutagen.Bethesda.Oblivion;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace TMSpeedPatcher;
+
+public class Settings
+{
+    [MaintainOrder]
+
+    [SettingName("Speed Multiplier")]
+    [Tooltip("Multiplies each npc and creature's speed")]
+    public float SpeedMult = 1.2f;
+
+    [SettingName("NPC Blacklist")]
+    [Tooltip("These NPCs will not be adjusted")]
+    public HashSet<IFormLinkGetter<INpcGetter>> NpcBlacklist = new();
+
+    [SettingName("Creature Blacklist")]
+    [Tooltip("These NPCs will not be adjusted")]
+    public HashSet<IFormLinkGetter<ICreatureGetter>> CreatureBlacklist = new();
+}
